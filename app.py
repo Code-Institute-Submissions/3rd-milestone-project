287ec4ad1be243680e2c4c17be4ed833899e6c5a
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-05-25 18:35:45 +0200

    Made connection to local MongoDB

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template, url_for
+from pymongo import MongoClient
 
 # instantiated flask application
 app = Flask(__name__)
 
-recipe_list = [
-    {
-        "user" : "BBQ fan",
-        "title" : "The best BBQ burger",
-        "description" : "We like our burgers piled sky-high. This version comes with our secret sauce (made from four very simple ingredients), sweet and sour onions, emmental cheese, gherkins, frilly lettuce, and more."
-    },
-        {
-        "user" : "Another BBQ fan",
-        "title" : "BBQ pulled pork",
-        "description" : "Pulled pork is a great recipe to feed a crowd at a summer BBQ. Ours are served up in soft buns with crunchy coleslaw – plus we've included extra instructions on how to make this recipe in a slow cooker."
-    }
-]
+client = MongoClient('localhost', 27017)
+db = client.recipe
+collection = db.recipe
+
 
 @app.route("/")
 @app.route("/home")
@@ -25,7 +18,7 @@ def home():
 
 @app.route("/recipes")
 def recipes():
-    return render_template('recipes.html', recipe_list = recipe_list)
+    return render_template('recipes.html', recipe_list = collection.find())
 
 
 if __name__ == "__main__":
