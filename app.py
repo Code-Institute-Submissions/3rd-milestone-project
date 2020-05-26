698728906833b3fdafd79a0d162baa3fbd65cea1
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-05-26 22:13:54 +0200

    Added registration page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from pymongo import MongoClient
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
 
 # instantiated flask application
 app = Flask(__name__)
@@ -27,7 +27,6 @@ def recipes():
 def account():
     return render_template('account.html', title = 'User account')
 
-
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -38,6 +37,14 @@ def login():
         else:
             flash('Login failed. Please make sure you used the correct username (= e-mail) and password!', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Welcome, we\'re glad to have you here {form.first_name.data}!', 'success')
+        return redirect(url_for('account'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
