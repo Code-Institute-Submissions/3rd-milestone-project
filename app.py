6bba6cebb75c3146184cb9d01b39b92de1b63465
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-05-26 19:42:32 +0200

    Added login functionality with flash messages

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
 from pymongo import MongoClient
+from forms import LoginForm
 
 # instantiated flask application
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '86f41a39c3a243fd22d96228eaeb23a60df36e76'
+
+# local MongoDB
 client = MongoClient('localhost', 27017)
 db = client.recipe
 collection = db.recipe
-
 
 @app.route("/")
 @app.route("/home")
@@ -20,6 +23,21 @@ def home():
 def recipes():
     return render_template('recipes.html', recipe_list = collection.find())
 
+@app.route("/account")
+def account():
+    return render_template('account.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'user@bbq.com' and form.password.data == 'password':
+            flash('You are succesfully logged in!', 'success')
+            return redirect(url_for('account'))
+        else:
+            flash('Login failed. Please make sure you used the correct username (= e-mail) and password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Keep me signed in')
+    submit = SubmitField('Sign in')
+
+
+
