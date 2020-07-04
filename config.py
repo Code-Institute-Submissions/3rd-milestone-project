fc329120ef605bb4c3e908b387412ca97ac67df9
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-07-04 20:05:51 +0200

    Removed os from config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-class Config(object):
+.class Config(object):
     SECRET_KEY          = os.environ.get('SECRET_KEY')
     MONGO_DBNAME        = os.environ.get('DB_NAME')           
     MONGO_URL           = os.environ.get('MONGO_URI')   
